3f4c6ca7bb2b26b063b0ae58ca3a45a80c2c898f
yegor smertenko <ye.smertenko@gmail.com>
2024-04-18 12:54:19 -0700

    feat(tide): add 2 themes

--- .config/fish/conf.d/tide.fish
+++ .config/fish/conf.d/tide.fish
@@ -1,31 +1,70 @@
-set -g tide_git_bg_color a9b665
-set -g tide_git_bg_color_unstable e78a4e
-set -g tide_git_bg_color_urgent ea6962
-set -g tide_git_branch_color 000000
-set -g tide_git_color_branch 000000
-set -g tide_git_color_conflicted 000000
-set -g tide_git_color_dirty 000000
-set -g tide_git_color_operation 000000
-set -g tide_git_color_staged 000000
-set -g tide_git_color_stash 000000
-set -g tide_git_color_untracked 000000
-set -g tide_git_color_upstream 000000
-set -g tide_git_conflicted_color 000000
-set -g tide_git_dirty_color 000000
+# Lean green theme ---------------------
+# git colors for minimal theme 
+# set -g tide_git_color_branch E1BE81
+# set -g tide_git_color_conflicted e45a69
+# set -g tide_git_color_untracked 80b4ca
+# set -g tide_git_color_dirty f98540
+# set -g tide_git_operation_color 957FB8
+# set -g tide_git_staged_color 8FC34E
+# set -g tide_git_stash_color 80838F
+# set -g tide_git_color_upstream 8FC34E 
+# path color in right component
+# set -g tide_pwd_color_anchors 8EAF65
+# set -g tide_pwd_color_dirs 677F6B
+# left component
+# set -g tide_time_color 677F6B
+# set -g tide_status_color C0A36E
+# set -g tide_go_color 80B4CA
+
+#---------------------------------------
+
+# Blurred green kanagawa-like theme ----
+# git colors
+set -g tide_git_bg_color 373941
+set -g tide_git_bg_color_unstable 373941
+set -g tide_git_color_branch C0A36E
+set -g tide_git_color_conflicted e45a69
+set -g tide_git_color_untracked C0A36E
+set -g tide_git_color_dirty f98540
+set -g tide_git_operation_color 957FB8
+set -g tide_git_staged_color 8FC34E
+set -g tide_git_color_upstream 92B468
+set -g tide_git_stash_color 80838F
 set -g tide_git_icon 
-set -g tide_git_operation_color 000000
-set -g tide_git_staged_color 000000
-set -g tide_git_stash_color 000000
-set -g tide_git_untracked_color 000000
-set -g tide_git_upstream_color 000000
+# right component bg color
+set -g tide_pwd_bg_color 677F6B
+# path colors in right component
+set -g tide_pwd_color_dirs 3A3A3A
+set -g tide_pwd_color_anchors 3A3A3A
+# left comoponent bg color
+set -g tide_time_bg_color 677F6B
+set -g tide_toolbox_bg_color 373941
+set -g tide_go_bg_color 373941
+set -g tide_go_color 7AABC0
+#---------------------------------------
 
-set -g tide_pwd_bg_color 444444
-set -g tide_character_color 957FB8
+# tide '>' char color
+set -g tide_character_color 5AA266
+set -g tide_character_color_failure DE2036
 
-set -g tide_character_color 957FB8
-set -g tide_character_color_failure C34043
 
-set -g tide_status_bg_color_failure E82424
+# set -g tide_git_bg_color a9b665
+# set -g tide_git_bg_color_unstable e78a4e
+# set -g tide_git_bg_color_urgent ea6962
+# set -g tide_git_color_branch 000000
+# set -g tide_git_color_operation 000000
+# set -g tide_git_color_staged 000000
+# set -g tide_git_color_stash 000000
+# set -g tide_git_color_upstream 000000
+# set -g tide_git_conflicted_color 000000
+# set -g tide_git_dirty_color 000000
 
-set -g tide_cmd_duration_bg_color 7FB4CA
-set -g tide_cmd_duration_threshold 2000
+
+
+# set -g tide_character_color 957FB8
+#
+# set -g tide_status_bg_color_failure E82424
+
+# set -g tide_cmd_duration_bg_color 7FB4CA
+# set -g tide_cmd_duration_threshold 2000
+
--- .config/fish/functions/add_to_path.fish
+++ .config/fish/functions/add_to_path.fish
@@ -1,11 +1,7 @@
 function add_to_path
-  if set -q argv[1] ; and test -d "$argv[1]"
-    set -l target_path (path resolve $argv[1])
-    set -gx PATH $PATH $target_path
-    echo "Added $target_path to \$PATH."
-  else
-    echo "Please provide a valid directory path as an argument."
-    return 1
-  end
-end
+  set -l p $(pwd)/$argv[1]
+  set -gx PATH $p $PATH 
+  echo "Added $p to PATH"
+end 
+
 
